2c9f1b7a8482aa5c5dfb165de8531b3f0efce18a
pxhanus <pxhanus@gmail.com>
2015-11-17 14:55:28 -0600

    Initial commit

---
+++ ReduceFraction/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNumerator = New System.Windows.Forms.TextBox()
+        Me.txtDenominator = New System.Windows.Forms.TextBox()
+        Me.btnReduce = New System.Windows.Forms.Button()
+        Me.lblReduced = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter numerator: "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(25, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(99, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter denominator: "
+        '
+        'txtNumerator
+        '
+        Me.txtNumerator.Location = New System.Drawing.Point(130, 29)
+        Me.txtNumerator.Name = "txtNumerator"
+        Me.txtNumerator.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumerator.TabIndex = 2
+        '
+        'txtDenominator
+        '
+        Me.txtDenominator.Location = New System.Drawing.Point(130, 72)
+        Me.txtDenominator.Name = "txtDenominator"
+        Me.txtDenominator.Size = New System.Drawing.Size(100, 20)
+        Me.txtDenominator.TabIndex = 3
+        '
+        'btnReduce
+        '
+        Me.btnReduce.Location = New System.Drawing.Point(28, 113)
+        Me.btnReduce.Name = "btnReduce"
+        Me.btnReduce.Size = New System.Drawing.Size(75, 27)
+        Me.btnReduce.TabIndex = 4
+        Me.btnReduce.Text = "Reduce"
+        Me.btnReduce.UseVisualStyleBackColor = True
+        '
+        'lblReduced
+        '
+        Me.lblReduced.AutoSize = True
+        Me.lblReduced.Location = New System.Drawing.Point(127, 120)
+        Me.lblReduced.Name = "lblReduced"
+        Me.lblReduced.Size = New System.Drawing.Size(0, 13)
+        Me.lblReduced.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 173)
+        Me.Controls.Add(Me.lblReduced)
+        Me.Controls.Add(Me.btnReduce)
+        Me.Controls.Add(Me.txtDenominator)
+        Me.Controls.Add(Me.txtNumerator)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Reduce Fraction"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtNumerator As System.Windows.Forms.TextBox
+    Friend WithEvents txtDenominator As System.Windows.Forms.TextBox
+    Friend WithEvents btnReduce As System.Windows.Forms.Button
+    Friend WithEvents lblReduced As System.Windows.Forms.Label
+
+End Class
---
+++ ReduceFraction/Form1.vb
@@ -0,0 +1,35 @@
+﻿'Patricia Hanus
+'151117
+'ReduceFraction
+'_____________________________________________________________________________
+Public Class Form1
+
+    Private Sub btnReduce_Click(sender As Object, e As EventArgs) Handles btnReduce.Click
+        Dim numerator As Integer = Val(Me.txtNumerator.Text)
+        Dim denominator As Integer = Val(Me.txtDenominator.Text)
+        Dim reduceNum As Integer
+        Dim reduceDenom As Integer
+        Dim temp As Integer = Val(Me.txtDenominator.Text)
+
+        Call Reduce(numerator, denominator, reduceNum, reduceDenom, temp)
+
+        Me.lblReduced.Text = numerator & "/" & denominator & " is reduced to " & reduceNum & "/" & reduceDenom
+    End Sub
+    '********************************************************************************************
+    'Reduce
+    'Finds greatest common denominator
+    '
+    'post: returns reduced numerator and denominator
+    '********************************************************************************************
+    Sub Reduce(ByVal numerator As Integer, ByVal demoninator As Integer, ByRef reduceNum As Integer, _
+               ByRef reduceDenom As Integer, ByRef temp As Integer)
+
+        Do Until (numerator Mod temp) = 0 And (demoninator Mod temp) = 0
+            temp -= 1
+        Loop
+
+        reduceNum = numerator / temp
+        reduceDenom = demoninator / temp
+
+    End Sub
+End Class
